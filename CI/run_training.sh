0461eb0089e4750a4fe14f27d9812a610442283f
Christopher Brown <chris.brown@fpsl.net>
2025-04-11 19:35:22 +0200

    Copy models around

--- CI/run_training.sh
+++ CI/run_training.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 if [[ "$1" == "False" ]]; then
     python tagger/train/train.py -n $Name -p 50 
+    python tagger/train/train.py --plot-basic -n $Name
     export MODEL_LOCATION=${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/model 
-python tagger/train/train.py --plot-basic -n $Name
+    cd output/baseline
+    eos mkdir -p ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/model
+    eos cp model/saved_model.h5 ${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/model/saved_model.h5 .
+    export MODEL_LOCATION=${EOS_STORAGE_DIR}/${EOS_STORAGE_SUBDIR}/model
+else
+    cd output/baseline
+    eos cp ${MODEL_LOCATION}/saved_model.h5 .
+    python tagger/train/train.py --plot-basic -n $Name
 
 
 
