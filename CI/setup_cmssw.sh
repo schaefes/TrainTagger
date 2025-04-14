dd9bf0b57de471e54881a9b976d3cfbc9bd5469b
Christopher Brown <chris.brown@fpsl.net>
2025-04-14 11:47:44 +0200

    15 1 migration (#36)
    * Update ml_pipeline.yml to 15_1
    
    * Update .gitlab-ci.yml to 15_1
    
    * Update setup_cmssw.sh to 15_1
    
    * Add track finding parameters
    
    * Add track parameters to jetntuple
    
    * Proper env variables
    
    * Save correct file
    
    * Add variable name root file
    
    * Update setup_cmssw.sh with proper sed
    
    * Switch to cms-hls4ml model
    
    * Update ml_pipeline.yml to 15_1
    
    * Update .gitlab-ci.yml to 15_1
    
    * Update setup_cmssw.sh to 15_1
    
    * Add track finding parameters
    
    * Add track parameters to jetntuple
    
    * Proper env variables
    
    * Save correct file
    
    * Add variable name root file
    
    * Update setup_cmssw.sh with proper sed
    
    * Switch to cms-hls4ml model

--- CI/setup_cmssw.sh
+++ CI/setup_cmssw.sh
@@ -38,10 +38,8 @@ git config user.name "Chriisbrown"
 
 
 
-git clone --quiet https://github.com/CMS-L1T-Jet-Tagging/hls4ml-jettagger.git 
-mv hls4ml-jettagger L1TSC4NGJetModel
+git clone --quiet https://github.com/cms-hls4ml/L1TSC4NGJetModel.git
 cd L1TSC4NGJetModel
-git checkout L1TSC4NGJetModel
 
 cp -r ../../../tagger/firmware/L1TSC4NGJetModel/firmware L1TSC4NGJetModel/
 ./setup.sh
@@ -50,7 +48,7 @@ make
 make install
 cd ..
 
-git clone https://github.com/CMS-L1T-Jet-Tagging/FastPUPPI.git -b 14_2_0/L1TSC4NGJetTagger
+git clone https://github.com/CMS-L1T-Jet-Tagging/FastPUPPI.git -b 15_1_0/L1TSC4NGJetTagger
 
 
 if [[ "$COMPILE" == "false" ]]; then exit 0; fi
@@ -66,7 +64,10 @@ if [[ "$RUN" == "false" ]]; then exit 0; fi
 
 cd FastPUPPI/NtupleProducer/python
 cmsenv
-
+echo ${TRACK_ALGO}
+echo  ${N_PARAMS}
+sed -i -e 's/trktype = "extended"/trktype = "'${TRACK_ALGO}'"/g' runJetNTuple.py
+sed -i -e 's/nparam = 5/nparam = '${N_PARAMS}'/g' runJetNTuple.py
 echo "Temporary workaround to get the input files"
 #curl -s https://cerminar.web.cern.ch/cerminar/data/14_0_X/fpinputs_131X/v3/TTbar_PU200/inputs131X_1.root -o inputs131X_1.root
 #echo '\nprocess.source.fileNames = ["file:inputs131X_1.root"]' >> runJetNTuple.py
